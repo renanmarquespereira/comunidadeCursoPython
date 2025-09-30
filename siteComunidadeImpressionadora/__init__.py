f0d18ca3053b6f50081c8df6be77910f28cda2d0
renanmarquespereira <163781666+renanmarquespereira@users.noreply.github.com>
2025-09-30 15:31:20 -0300

    Update __init__.py

--- siteComunidadeImpressionadora/__init__.py
+++ siteComunidadeImpressionadora/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
@@ -9,7 +10,10 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "7fff09fe31cb95069ca7515bd5022585"
 
 # Banco de dados
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 database = SQLAlchemy(app)
 
