6de95b448b74d35f25683cebbcbc39f154902918
renanmarquespereira <renanmarques31@gmail.com>
2025-10-22 07:52:54 -0300

    projetoSite

---
+++ main.py
@@ -0,0 +1,4 @@
+from siteComunidadeImpressionadora import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ siteComunidadeImpressionadora/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import os
+import sqlalchemy
+
+app = Flask(__name__)
+
+#Token de seguranca
+app.config["SECRET_KEY"] = "7fff09fe31cb95069ca7515bd5022585"
+
+# Banco de dados
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+
+database = SQLAlchemy(app)
+
+cripitografarSenha = Bcrypt(app)
+
+loginManager = LoginManager()
+loginManager.init_app(app)
+loginManager.login_view = "login"
+loginManager.login_message = "Faça o login para acessar a pagina !"
+loginManager.login_message_category = "alert-info"
+
+from siteComunidadeImpressionadora import models
+
+engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+inspector = sqlalchemy.inspect(engine)
+
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("BASE DE DADOS CRIADA")
+else:
+    print("BASE DE DADOS JA EXISTENTE")
+
+# importar arquivos de links
+from siteComunidadeImpressionadora import routes
