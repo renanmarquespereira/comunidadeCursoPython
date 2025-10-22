ea178fafb6547dde476bafd77821268c6a138bd2
renanmarquespereira <renanmarques31@gmail.com>
2025-10-22 08:06:15 -0300

    projetoSite

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 from siteComunidadeImpressionadora import app
 
 if __name__ == '__main__':
-=======
-from siteComunidadeImpressionadora import app
-
-if __name__ == '__main__':
->>>>>>> 1723ee4999c69d256e2aed76b0d392e4406e9091
     app.run(debug=True)
