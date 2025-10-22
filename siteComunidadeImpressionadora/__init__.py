0ef4dca90fe6d652b000f9244d0eb867c2ee4c47
renanmarquespereira <renanmarques31@gmail.com>
2025-10-22 08:24:15 -0300

    projetoSite

--- siteComunidadeImpressionadora/__init__.py
+++ siteComunidadeImpressionadora/__init__.py
@@ -7,7 +7,7 @@ import sqlalchemy
 
 app = Flask(__name__)
 
-#Token de seguranca
+# Token de segurança
 app.config["SECRET_KEY"] = "7fff09fe31cb95069ca7515bd5022585"
 
 # Banco de dados
@@ -17,7 +17,6 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 
 database = SQLAlchemy(app)
-
 cripitografarSenha = Bcrypt(app)
 
 loginManager = LoginManager()
@@ -26,6 +25,7 @@ loginManager.login_view = "login"
 loginManager.login_message = "Faça o login para acessar a pagina !"
 loginManager.login_message_category = "alert-info"
 
+# ⚠️ Importar models só depois de configurar o banco
 from siteComunidadeImpressionadora import models
 
 engine = sqlalchemy.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
@@ -39,5 +39,5 @@ if not inspector.has_table("usuario"):
 else:
     print("BASE DE DADOS JA EXISTENTE")
 
-# importar arquivos de links
+# Importar rotas por último
 from siteComunidadeImpressionadora import routes
